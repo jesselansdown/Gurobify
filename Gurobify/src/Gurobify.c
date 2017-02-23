d6fcefd02c9c7ea40e374806071fac915cb9a934
Jesse Lansdown <jesselansdown@gmail.com>
2017-02-24 00:13:01 +0100

    GurobiSolve now returns [optimisation status, objective value]. Added a number of error checks.

--- Gurobify/src/Gurobify.c
+++ Gurobify/src/Gurobify.c
@@ -1,24 +1,67 @@
 /*
  * Gurobify: Gurobify provides an interface to Gurobi from GAP.
+ * Jesse Lansdown
  */
 
 #include "src/compiled.h"          /* GAP headers */
 #include "gurobi_c.h"
+#include <stdlib.h>
+#include <stdio.h>
 
 
 Obj GurobiSolve(Obj self, Obj lp_file)
 {
+
+	//TODO: Add option of suppressing the output to the screen during optimization
+	//TODO: Return the actual solution to the problem
+	//TODO: Add option of additional constraints
+	//TODO: Add time limit paramter
+	//TODO: Add numeric focus parameter - infact, all parameters?
+	
+
     GRBenv *env = NULL;
     GRBmodel *model = NULL;
+
+    int optimstatus;
+	double objval;
+    int error = 0;
+
     //TODO: Check that lp_file is actually a string
     char *lp_file_name = CSTR_STRING(lp_file);
-    int error = 0;
-    // We are not interested in a log file, so the second argument is NULL
-    error = GRBloadenv(&env, NULL);
+
+    error = GRBloadenv(&env, NULL);     // We are not interested in a log file, so the second argument of GRBloadenv is NULL
+    if (error || env == NULL) {
+        ErrorMayQuit( "Error: failed to create new environment.", 0, 0 );
+    }
+
     error = GRBreadmodel(env, lp_file_name, &model);
-    //TODO: check that the model was read correctly
+
+    if (error)
+        ErrorMayQuit( "Error: model was not read correctly.", 0, 0 );    
+
     error = GRBoptimize(model);
-    return 0;
+
+    if (error)
+        ErrorMayQuit( "Error: model was not able to be optimized", 0, 0 );
+
+    error = GRBgetintattr(model, GRB_INT_ATTR_STATUS, &optimstatus);
+    if (error)
+        ErrorMayQuit( "Error: unable to obtain optimisation status", 0, 0 );
+
+    error = GRBgetdblattr(model, GRB_DBL_ATTR_OBJVAL, &objval);			//TODO: First check that the optimstatus returned a success
+    if (error)
+        ErrorMayQuit( "Error: unable to obtain optimal value", 0, 0 );
+
+
+	Obj results = NEW_PLIST( T_PLIST , 2);
+	SET_LEN_PLIST( results , 2 );
+	SET_ELM_PLIST(results, 1, INTOBJ_INT(optimstatus));
+	SET_ELM_PLIST(results, 2, INTOBJ_INT(objval));			//TODO: This should be a float?
+  
+	GRBfreemodel(model);
+	GRBfreeenv(env);
+
+    return results;
 }
 
 Obj TestCommandWithParams(Obj self, Obj param, Obj param2)
