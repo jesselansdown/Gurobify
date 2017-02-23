4c2b0bcd34f2a8c89b9cdabb3b1d5d4de7d4c511
Jesse Lansdown <jesselansdown@gmail.com>
2017-02-23 13:04:59 +0100

    Initial Commit: Minimum working example for calling Gurobi from C++

---
+++ BasicGurobiSolver.cpp
@@ -0,0 +1,17 @@
+// This is intended to become an interface between Gurobi and GAP
+//
+// Author: Jesse Lansdown
+//
+// To compile run the following command
+// g++ -m64 -g -o G\(AP\)urobi G\(AP\)urobi.cpp -I /Library/gurobi701/mac64/include/ /Library/gurobi701/mac64/lib/libgurobi_c++.a /Library/gurobi701/mac64/lib/libgurobi70.so -lpthread -lm
+
+#include "gurobi_c++.h"
+using namespace std;
+
+int main() {
+	GRBEnv env = GRBEnv();
+	GRBModel model = GRBModel(env, "lp_file.lp");
+	model.optimize();
+	cout << "Optimal solution: " << model.get(GRB_DoubleAttr_ObjVal) << endl;
+	return 0;
+}
